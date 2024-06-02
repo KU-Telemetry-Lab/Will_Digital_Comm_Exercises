1a2fcf69a60d5056d946a678382d38edae9ae01e
powersthegreat <williampowers@ku.edu>
2024-06-02 15:47:52 -0500

    binary PAM completed

---
+++ binary_PAM/main.py
@@ -0,0 +1,110 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sys
+sys.path.insert(0, '../KUSignalLib/src')
+from KUSignalLib import DSP
+from KUSignalLib import MatLab
+
+
+A = 1  # incoming signal amplitude
+amplitude_to_bits = {-A: 0, A: 1} # binary PAM symbol to bits map
+bits_to_amplitude = {value: key for key, value in amplitude_to_bits.items()} # binary PAM bits to symbol map
+
+sample_rate = 16 # samples/bit
+pulse_shape = "NRZ" # non return to zero pulse shaping
+symbol_clock_offset = 0
+
+test_input_1 = [1]
+test_input_2 = [1, 0, 0, 1]
+test_input_3 = [1, 1, 1, 1, 0, 0, 0, 0]
+
+# 1.1 UPSAMPLE THE BASEBAND BINARY SIGNAL
+b_k = test_input_2 # input binary signal
+a_k = [bits_to_amplitude[bit] for bit in b_k] # symbol mapping
+a_k_upsampled = DSP.Upsample(a_k, sample_rate, interpolate=False) # upsampling
+
+
+# 1.2 NRZ FILTER THE UPSDAMPLED SIGNAL
+filter_num = [.25*1 for i in range(sample_rate)]
+filter_denom = [1]
+padding_length = max(len(filter_num), len(filter_denom))
+s_nT = DSP.DirectForm2(filter_num, filter_denom, a_k_upsampled)
+
+
+# 2.1 MATCH FILTER THE RECIEVED SIGNAL
+x_nT = DSP.DirectForm2(filter_num, filter_denom, s_nT) 
+
+
+# 2.2 DOWNSAMPLE EACH PULSE
+x_kTs = DSP.Downsample(x_nT[sample_rate+1:], sample_rate)
+
+
+# 2.3 MAKE A DECISION FOR EACH PULSE
+detected_bits = [amplitude_to_bits[round(symbol)] for symbol in x_kTs]
+
+# # 2.4 PlOTTING SIMULATION RESULTS
+# plt.figure()
+# plt.stem(a_k)
+# plt.title('a_k')
+# plt.xlabel('Index')
+# plt.ylabel('Value')
+
+# plt.figure()
+# plt.stem(a_k_upsampled)
+# plt.title('a_k_upsampled')
+# plt.xlabel('Index')
+# plt.ylabel('Value')
+
+# plt.figure()
+# plt.stem(s_nT)
+# plt.title('s_nT')
+# plt.xlabel('Index')
+# plt.ylabel('Value')
+
+# plt.figure()
+# plt.stem(x_nT)
+# plt.title('x_nT')
+# plt.xlabel('Index')
+# plt.ylabel('Value')
+
+# plt.figure()
+# plt.stem(x_kTs)
+# plt.title('x_kTs')
+# plt.xlabel('Index')
+# plt.ylabel('Value')
+
+# plt.figure()
+# plt.stem(detected_bits)
+# plt.title('x_nT')
+# plt.xlabel('Index')
+# plt.ylabel('Value')
+
+# plt.show()
+
+
+# 3.3 TEST SYSTEM ON GIVEN ASCII DATA
+test_file = "bb2data.mat" # binay input file with N messages
+input_message_length = 126 # bits (18 ASCII characters)
+r_nT = MatLab.loadMatLabFile(test_file)[1]
+
+filter_num = [.25*1 for i in range(sample_rate)]
+filter_denom = [1]
+padding_length = max(len(filter_num), len(filter_denom))
+x_nT = DSP.DirectForm2(filter_num, filter_denom, r_nT)
+
+x_kTs = DSP.Downsample(x_nT[sample_rate:], sample_rate) # may need +1 to sample rate index
+
+detected_bits = [amplitude_to_bits[round(symbol)] for symbol in x_kTs]
+
+segmented_arrays = [detected_bits[i:i+7] for i in range(0, len(detected_bits), 7)]
+
+bin_chars = []
+for segment in segmented_arrays:
+    binary_string = ''.join(str(bit) for bit in segment)
+    decimal_value = int(binary_string, 2)
+    ascii_char = chr(decimal_value)
+    bin_chars.append(ascii_char)
+
+print(''.join(bin_chars))
+
---
+++ phase_locked_loop/main.py
@@ -0,0 +1,58 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+import sys
+sys.path.insert(0, '../KUSignalLib/src')
+from KUSignalLib import DSP
+
+
+def complex_sinusoid(length, frequency, phase):
+    t = np.arange(length)
+    omega = 2 * np.pi * frequency / length
+    complex_signal = np.exp(1j * (omega * t + phase))
+    return complex_signal
+
+def loop_filter(x, Bn, Ts, zeta=1/math.sqrt(2), K0=1):
+    # x = input signal
+    # Bn = loop bandwidth
+    # Ts = symbol rate
+    # zeta = damping factor
+    # K0 = ?
+    K1 = ((4*zeta)/(zeta+(1/(4*zeta))))*((Bn*Ts)/K0)
+    K2 = ((4)/(zeta+(1/(4*zeta)))**2)*(((Bn*Ts)**2)/K0)
+
+    filter_num = [(K1+K2), K2]
+    filter_denom = [1]
+    y = DSP.DirectForm2(filter_num, filter_denom, x)
+    return y
+
+def dds(x, fc, K0=1):
+    # x = input signal
+    # fc = estimated carrier signal frequency
+    # K0 = ?
+    y = DDS
+
+
+
+
+
+length = 1000  # Length of the complex sinusoid
+frequency = 10  # Frequency of the sinusoid
+phase = 0  # Phase of the sinusoid
+complex_signal = complex_sinusoid(length, frequency, phase)
+
+# Plotting Complex Sinusoid
+plt.figure(figsize=(10, 5))
+
+plt.subplot(2, 1, 1)
+plt.plot(np.real(complex_signal), label='Real part')
+plt.plot(np.imag(complex_signal), label='Imaginary part')
+
+plt.subplot(2, 1, 2)
+plt.plot(np.abs(complex_signal), label='Magnitude')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
