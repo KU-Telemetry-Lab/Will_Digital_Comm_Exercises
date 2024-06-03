39886a61381f20413dfa5ffed307b767a877dc97
powersthegreat <williampowers@ku.edu>
2024-06-03 16:49:12 -0500

    still working on pll stuff

--- phase_locked_loop/main.py
+++ phase_locked_loop/main.py
@@ -14,44 +14,79 @@ def complex_sinusoid(length, frequency, phase):
     complex_signal = np.exp(1j * (omega * t + phase))
     return complex_signal
 
-def loop_filter(x, Bn, Ts, zeta=1/math.sqrt(2), K0=1):
-    # x = input signal
-    # Bn = loop bandwidth
-    # Ts = symbol rate
-    # zeta = damping factor
-    # K0 = ?
+
+################## PLL SPECIFIC FUNCTIONS #######################
+
+def phaseDetector(sample1, sample2, Kp=1):
+    """
+    Phase detector implementation.
+    """
+    return np.angle(sample2 / sample1, deg=False) * Kp
+
+def loopFilter(x, Bn, Ts, zeta=1/np.sqrt(2), K0=1):
+    """
+    Loop filter implementation (PI controller).
+    """
     K1 = ((4*zeta)/(zeta+(1/(4*zeta))))*((Bn*Ts)/K0)
     K2 = ((4)/(zeta+(1/(4*zeta)))**2)*(((Bn*Ts)**2)/K0)
-
-    filter_num = [(K1+K2), K2]
-    filter_denom = [1]
-    y = DSP.DirectForm2(filter_num, filter_denom, x)
+    
+    filter_num = np.array([K1 + K2, -K1], dtype=np.float64)
+    filter_denom = np.array([1, -1], dtype=np.float64)
+    y = np.convolve(x, filter_num, mode='same')
     return y
 
+dds_prev = 0  # dds implementation dependent on previous calculated value
+
 def dds(x, fc, K0=1):
-    # x = input signal
-    # fc = estimated carrier signal frequency
-    # K0 = ?
-    y = DDS
+    """
+    Direct Digital Synthesis (DDS) implementation.
+    """
+    global dds_prev
+    dds_prev += x + fc * 2 * np.pi
+    y = K0 * dds_prev
+    return np.cos(y) + 1j * np.sin(y)
 
+def complex_sinusoid(length, frequency, phase=0):
+    t = np.arange(length)
+    return np.exp(1j * (2 * np.pi * frequency * t + phase))
 
+#################################################################
+# Simulation parameters
+length = 1000
+fc = 10
+phase = 0
+test_input = complex_sinusoid(length, fc, phase)
 
-length = 1000  # Length of the complex sinusoid
-frequency = 10  # Frequency of the sinusoid
-phase = 0  # Phase of the sinusoid
-complex_signal = complex_sinusoid(length, frequency, phase)
+# System parameters
+Ts = 1/fc
+Bn = 0.005
+fc_estimated = 11
 
+# Recording parameters
+pll_input = []
+pll_output = [np.cos(fc_estimated) + 1j*np.sin(fc_estimated)]
+
+# Set up in streaming framework
+for i in range(len(test_input)):
+    e_nT = phaseDetector(test_input[i], pll_output[-1])
+    v_nT = loopFilter([e_nT], Bn, Ts)[-1]
+    dds_out = dds(v_nT, fc_estimated)
+
+    pll_input.append(test_input[i])
+    pll_output.append(dds_out)
+
+pll_input = np.array(pll_input)
+pll_output = np.array(pll_output)
+
+###################### PLOTTING ################################
 # Plotting Complex Sinusoid
 plt.figure(figsize=(10, 5))
-
-plt.subplot(2, 1, 1)
-plt.plot(np.real(complex_signal), label='Real part')
-plt.plot(np.imag(complex_signal), label='Imaginary part')
-
-plt.subplot(2, 1, 2)
-plt.plot(np.abs(complex_signal), label='Magnitude')
+plt.plot(np.real(test_input), label='Input Real part')
+plt.plot(np.real(pll_output[:-1]), label='Output Real part')
 plt.legend()
+plt.title('Input and Output of PLL')
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.grid(True)
 
-plt.tight_layout()
 plt.show()
-
