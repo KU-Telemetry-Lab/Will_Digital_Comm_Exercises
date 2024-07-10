de13ce0c15bc9817f6a741fc7ec870db9a0e2c5f
powersthegreat <williampowers@ku.edu>
2024-07-10 09:48:46 -0500

    kind of working phase sync

--- Carrier_Phase_Sync_QPSK/main.py
+++ Carrier_Phase_Sync_QPSK/main.py
@@ -55,7 +55,8 @@ string_input_bin = ''.join(string_to_ascii_binary(string_input))
 input_bin_blocks = [string_input_bin[i:i+2] for i in range(0, len(string_input_bin), 2)]
 test_input_3 = [int(bin2, 2) for bin2 in input_bin_blocks]
 
-header = (3 * np.ones(20, dtype=int)).tolist()
+# SYNCHRONIZATION PARAMETERS
+header = (3 * np.ones(100, dtype=int)).tolist()
 unique_word = [0, 1, 2, 3]
 phase_ambiguities = {
     "0123": 0,
@@ -64,10 +65,11 @@ phase_ambiguities = {
     "1302": 3*np.pi/2
 }
 
-transmitter_phase_offset = np.pi/5
+transmitter_phase_offset = np.pi
+transmitter_freq_offset =  0/sample_rate # (Hz)
 
 # 1.1 UPSAMPLE THE BASEBAND DISCRETE SYMBOLS
-b_k = header + unique_word + test_input_3
+b_k = header + unique_word + test_input_1
 a_k = [bits_to_amplitude[bit] for bit in b_k]
 a_k_upsampled = DSP.upsample(a_k, sample_rate, interpolate=False)
 a_k_upsampled_real = np.real(a_k_upsampled)
@@ -80,10 +82,10 @@ pulse_shape = communications.srrc(.5, sample_rate, length)
 s_nT_real = np.real(np.roll(DSP.convolve(a_k_upsampled_real, pulse_shape, mode="same"), -1))
 s_nT_imag = np.real(np.roll(DSP.convolve(a_k_upsampled_imag, pulse_shape, mode="same"), -1))
 
-# 1.3 MODULATE ONTO CARRIER USING LOCAL OSCILLATOR
+# 1.3 MODULATE ONTO CARRIER USING LOCAL OSCILLATOR (ADD PHASE OFFSET)
 s_nT_modulated = (
-    np.array(np.sqrt(2) * np.real(DSP.modulate_by_exponential(s_nT_real, carrier_frequency, sample_rate))) +
-    np.array(np.sqrt(2) * np.imag(DSP.modulate_by_exponential(s_nT_imag, carrier_frequency, sample_rate)))
+    np.array(np.sqrt(2) * np.real(DSP.modulate_by_exponential(s_nT_real, carrier_frequency + transmitter_freq_offset, sample_rate))) +
+    np.array(np.sqrt(2) * np.imag(DSP.modulate_by_exponential(s_nT_imag, carrier_frequency + transmitter_freq_offset, sample_rate)))
 ) * np.exp(1j * transmitter_phase_offset)
 
 # 2.1 DEMODULATE THE RECEIVED SIGNAL USING LOCAL OSCILLATOR
@@ -97,17 +99,17 @@ x_nT_imag = np.real(np.roll(DSP.convolve(r_nT_imag, pulse_shape, mode="same"), -
 # 2.3 DOWNSAMPLE EACH PULSE
 x_kTs_real = np.array(DSP.downsample(x_nT_real, sample_rate))
 x_kTs_imag = np.array(DSP.downsample(x_nT_imag, sample_rate))
-x_kTs = (x_kTs_real + 1j * x_kTs_imag)[len(header):] # remove header
+x_kTs = (x_kTs_real + 1j * x_kTs_imag) # remove header
 
-# PLL SYSTEM PARAMETERS
-B = 0.02 * sample_rate
+# # DEBUGGING!!!
+# detected_ints = communications.nearest_neighbor(x_kTs, qpsk_constellation)
+# print(f"transmitted symbols: {b_k[len(header):]}")
+# print(f"detected symbols: {detected_ints}")
+
+# 3.1 PLL SYSTEM PARAMETERS
+B = 0.005 * sample_rate
 zeta = 1 / np.sqrt(2)
-K0 = 1
-Kp = 1
-K1 = ((4 * zeta) / (zeta + (1 / (4 * zeta)))) * ((B * (1 / sample_rate)) / K0)
-K2 = ((4) / (zeta + (1 / (4 * zeta))) ** 2) * (((B * (1 / sample_rate)) ** 2) / K0)
-
-pll = PLL.PLL(Kp, K0, K1, K2, carrier_frequency, sample_rate)
+pll = PLL.PLL(B, zeta, carrier_frequency, sample_rate)
 
 dds_output = np.exp(1j * 0)
 rotated_constellations = []
@@ -134,30 +136,54 @@ for i in range(len(x_kTs)):
     # generate next dds output
     dds_output = np.exp(1j * loop_filter_output)
 
-# unique word resolution
-received_unique_word = communications.nearest_neighbor(detected_constellations[: len(unique_word)], qpsk_constellation)
-received_unique_word_string = ""
-for symbol in received_unique_word:
-    received_unique_word_string += str(symbol)
-
-detected_constellations = np.array(detected_constellations) * np.exp(-1j * phase_ambiguities[received_unique_word_string])
-
-detected_ints = communications.nearest_neighbor(detected_constellations[len(unique_word):], qpsk_constellation)
-print(f"Transmission Symbol Errors: {error_count(b_k[len(header) + len(unique_word):], detected_ints)}")
-
-plt.plot(np.real(rotated_constellations), np.imag(rotated_constellations), 'ro')
-plt.plot(np.real(detected_constellations), np.imag(detected_constellations), 'bo')
+# DEBUGGING!!!
+plt.plot(pll_error, label='phase error')
+plt.legend()
 plt.show()
 
-# print(f"b_k: {b_k[len(header) + len(unique_word):]}")
-# print(f"detected_ints: {detected_ints}")
+#####################
 
-# print(f"x_kTs: {x_kTs[len(unique_word):]}")
-# print(f"detected_constellations: {detected_constellations[len(unique_word):]}")
+# # remove header
+# detected_constellations = detected_constellations[len(header):]
 
-detected_bits = []
-for symbol in detected_ints:
-    detected_bits += ([*bin(symbol)[2:].zfill(2)])
+# # DEBUGGING!!!
+# # detected_ints = communications.nearest_neighbor(detected_constellations, qpsk_constellation)
+# # print(f"transmitted symbols: {b_k[len(header):]}")
+# # print(f"detected symbols: {detected_ints}")
 
-message = communications.bin_to_char(detected_bits)
+# # print(f"transmitted constellations: {a_k[len(header):]}")
+# # print(f"detected constellations: {detected_constellations}")
+
+# # 3.2 UNIQUE WORD RESOLUTION
+# received_unique_word = communications.nearest_neighbor(detected_constellations[:len(unique_word)], qpsk_constellation)
+# received_unique_word_string = ""
+# for symbol in received_unique_word:
+#     received_unique_word_string += str(symbol)
+
+# # 3.3 UNIQUE WORD OFFSET ADJUSTMENT
+# detected_constellations = np.array(detected_constellations) * np.exp(-1j * phase_ambiguities[received_unique_word_string])
+
+# # # DEBUGGING!!!
+# # print(f"transmitted constellations: {a_k[len(header):]}")
+# # print(f"detected constellations: {detected_constellations}")
+
+# # 4.1 SYMBOL DETECTION AND ERROR CALCULATION
+# detected_ints = communications.nearest_neighbor(detected_constellations[len(unique_word):], qpsk_constellation)
+# print(f"Transmission Symbol Errors: {error_count(b_k[len(header) + len(unique_word):], detected_ints)}")
+
+# # 4.2 CONSTELLATION PLOTTING
+# # plt.plot(np.real(rotated_constellations), np.imag(rotated_constellations), 'ro')
+# # plt.plot(np.real(detected_constellations), np.imag(detected_constellations), 'bo')
+# # plt.show()
+
+# # # DEBUGGING!!!
+# # print(f"transmitted symbols: {b_k[len(header) + len(unique_word):]}")
+# # print(f"detected symbols: {detected_ints}")
+
+# # 4.3 MESSAGE DECODING (BIN TO ASCII)
+# detected_bits = []
+# for symbol in detected_ints:
+#     detected_bits += ([*bin(symbol)[2:].zfill(2)])
+
+# message = communications.bin_to_char(detected_bits)
 print(message)
