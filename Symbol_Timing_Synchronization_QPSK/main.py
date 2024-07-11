1849e1c5e01bd7550b7ef342d711664fd3125af1
Powers <w656p201@home.ku.edu>
2024-07-11 15:26:21 -0500

    closer and closer to success

--- Symbol_Timing_Synchronization_QPSK/main.py
+++ Symbol_Timing_Synchronization_QPSK/main.py
@@ -8,7 +8,6 @@ sys.path.insert(0, '../KUSignalLib/src')
 from KUSignalLib import DSP
 from KUSignalLib import communications
 
-
 def string_to_ascii_binary(string, num_bits=7):
     ascii_binary_strings = []
     for char in string:
@@ -39,22 +38,91 @@ def interpolate(x, n, mode="linear"):
     interpolated_signal = interpolation_function(new_indices)
     return interpolated_signal
 
-def gardner_ted(received_signal, samples_per_symbol):
-    error = []
-    for i in range(samples_per_symbol, len(received_signal) - samples_per_symbol, samples_per_symbol):
-        early_sample = received_signal[i - samples_per_symbol // 2]
-        on_time_sample = received_signal[i]
-        late_sample = received_signal[i + samples_per_symbol // 2]
+###########################################################
 
-        timing_error = np.real(early_sample * (on_time_sample.conj() - late_sample.conj()))
-        error.append(timing_error)
+class SCS:
+    def __init__(self, input_signal_complex, samples_per_symbol, interpolation_factor):
+        self.input_signal_real = np.real(input_signal_complex)
+        self.input_signal_imag = np.imag(input_signal_complex)
+        self.samples_per_symbol = samples_per_symbol
+        self.counter = samples_per_symbol # start at sample
+        self.timing_error = 0
+        self.interpolation_factor = interpolation_factor
 
-    return np.array(error)
+        self.LFK2Prev = 0
+        self.adjusted_symbol_block_real = [0, 0, 0]
+        self.adjusted_symbol_block_imag = [0, 0, 0]
+
+        self.timing_error_record = []
+        self.scs_output_real = []
+        self.scs_output_imag = []
+
+    def get_timing_error(self):
+        return self.timing_error_record
+
+    def early_late_ted(self):
+        timing_error = self.adjusted_symbol_block_real[1] * (self.adjusted_symbol_block_real[2] - self.adjusted_symbol_block_real[0]) + self.adjusted_symbol_block_imag[1] * (self.adjusted_symbol_block_imag[2] - self.adjusted_symbol_block_imag[0])
+        self.timing_error_record.append(timing_error)
+        if timing_error >= 1:
+            timing_error = .99
+        return timing_error
+
+    def upsample(self, symbol_block):
+        return DSP.upsample(symbol_block, self.interpolation_factor, interpolate=False)
+
+    def interpolate(self, symbol_block):
+        return interpolate(symbol_block, self.interpolation_factor, mode="linear")
+
+    def loop_filter(self):
+        pass
+
+    def runner(self):
+        for i in range(len(self.input_signal_real)):
+            if self.counter == sample_rate:
+                # splice symbol block
+                if i == 0:
+                    symbol_block_real = np.concatenate((np.zeros(1), self.input_signal_real[i:i+2])) # early, on time, late
+                    symbol_block_imag = np.concatenate((np.zeros(1), self.input_signal_imag[i:i+2]))
+                else:
+                    symbol_block_real = self.input_signal_real[i-1:i+2]
+                    symbol_block_imag = self.input_signal_imag[i-1:i+2]
+            
+                self.timing_error = self.early_late_ted()
+
+                symbol_block_real_upsampled = self.upsample(symbol_block_real)
+                symbol_block_imag_upsampled = self.upsample(symbol_block_imag)
+
+                symbol_block_real_interpolated = self.interpolate(symbol_block_real_upsampled)
+                symbol_block_imag_interpolated = self.interpolate(symbol_block_imag_upsampled)
+
+                on_time_index = self.interpolation_factor + int(self.timing_error * self.interpolation_factor)
+
+                self.scs_output_real.append(symbol_block_real_interpolated[on_time_index])
+                self.scs_output_imag.append(symbol_block_imag_interpolated[on_time_index])
+
+                self.adjusted_symbol_block_real = [
+                    float(symbol_block_real_interpolated[on_time_index - self.interpolation_factor]),
+                    float(symbol_block_real_interpolated[on_time_index]),
+                    float(symbol_block_real_interpolated[on_time_index + self.interpolation_factor])
+                ]
+
+                self.adjusted_symbol_block_imag = [
+                    float(symbol_block_imag_interpolated[on_time_index - self.interpolation_factor]),
+                    float(symbol_block_imag_interpolated[on_time_index]),
+                    float(symbol_block_imag_interpolated[on_time_index + self.interpolation_factor])
+                ]
+
+                self.counter = 0 # reset counter
+            self.counter +=1 # increment counter
+        return np.array(self.scs_output_real) + 1j * np.array(self.scs_output_imag)
+
+
+###########################################################
 
 # SYSTEM PARAMETERS
 sample_rate = 8
 carrier_frequency = 0.25 * sample_rate
-symbol_clock_offset = 0
+symbol_clock_offset = .1
 qpsk_constellation = [[complex(np.sqrt(1) + np.sqrt(1) * 1j), 3],
                       [complex(np.sqrt(1) + -np.sqrt(1) * 1j), 2],
                       [complex(-np.sqrt(1) + -np.sqrt(1) * 1j), 0],
@@ -67,7 +135,7 @@ amplitude_to_bits = dict(zip(amplitudes, bits))
 bits_to_amplitude = dict(zip(bits, amplitudes))
 bits_to_bits_str = dict(zip(bits, bits_str))
 
-test_input_1 = [1, 0, 1, 0]
+test_input_1 = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
 test_input_2 = [3, 2, 1, 0, 1, 2, 3]
 string_input = "will is cool, this is a test"
 string_input_bin = ''.join(string_to_ascii_binary(string_input))
@@ -85,7 +153,7 @@ a_k_upsampled_real = np.real(a_k_upsampled)
 a_k_upsampled_imag = np.imag(a_k_upsampled)
 
 # 1.2 INTRODUCE TIMING OFFSET
-timing_offset = 0.2  # fractional offset in symbols
+timing_offset = 0.6 # fractional offset in symbols
 def fractional_delay(signal, delay):
     n = np.arange(len(signal))
     delayed_signal = np.interp(n - delay, n, signal)
@@ -104,49 +172,30 @@ s_nT_imag = np.real(np.roll(DSP.convolve(a_k_upsampled_imag, pulse_shape, mode="
 # 2.1 PULSE SHAPE (RECEIVE) AND REMOVE HEADER AND TAIL
 s_nT_real = np.real(np.roll(DSP.convolve(s_nT_real, pulse_shape, mode="same"), -1))[len(header)*sample_rate:-len(header)*sample_rate]
 s_nT_imag = np.real(np.roll(DSP.convolve(s_nT_imag, pulse_shape, mode="same"), -1))[len(header)*sample_rate:-len(header)*sample_rate]
+s_nT = s_nT_real + 1j * s_nT_imag
 
 # 2.2 SYMBOL TIMING ERROR CORRECTION
-timing_sync_output_real = []
-timing_sync_output_imag = []
+scs = SCS(s_nT, sample_rate, interpolation_factor=10)
+x_kTs = scs.runner()
 
-symbol_block_real = None 
-symbol_block_real_upsampled = None
-symbol_block_real_interpolated = None
-
-counter = sample_rate # start at sample
-tau = -timing_offset
-upsample_factor = 10
-
-for i in range(len(s_nT_real)):
-    if counter == sample_rate:
-        if i == 0:
-            symbol_block_real = np.concatenate((np.zeros(1), s_nT_real[i:i+2])) # early, on time, late
-            symbol_block_imag = np.concatenate((np.zeros(1), s_nT_imag[i:i+2]))
-        else:
-            symbol_block_real = s_nT_real[i-1:i+2]
-            symbol_block_imag = s_nT_imag[i-1:i+2]
-
-        symbol_block_real_upsampled = DSP.upsample(symbol_block_real, upsample_factor, interpolate=False)
-        symbol_block_imag_upsampled = DSP.upsample(symbol_block_imag, upsample_factor, interpolate=False)
-
-
-        symbol_block_real_interpolated = interpolate(symbol_block_real_upsampled, upsample_factor, mode="linear")
-        symbol_block_imag_interpolated = interpolate(symbol_block_imag_upsampled, upsample_factor, mode="linear")
-    
-        output_sample_real = symbol_block_real_interpolated[upsample_factor + int(tau*upsample_factor)]
-        output_sample_imag = symbol_block_imag_interpolated[upsample_factor + int(tau*upsample_factor)]
-
-        timing_sync_output_real.append(output_sample_real)
-        timing_sync_output_imag.append(output_sample_imag)
-
-        counter = 0 # reset counter
-    counter += 1
-
-x_kTs = np.array(timing_sync_output_real) + 1j * np.array(timing_sync_output_imag)
-
-# 2.5 MAKE A DECISION FOR EACH PULSE
+# 2.3 MAKE A DECISION FOR EACH PULSE
 detected_ints = communications.nearest_neighbor(x_kTs, qpsk_constellation)
-print(f"Transmission Symbol Errors: {error_count(b_k[len(header):-len(header)], detected_ints)}")
+error_count = error_count(b_k[len(header):-len(header)], detected_ints)
+print(f"Transmission Symbol Errors: {error_count}")
+print(f"Bit Error Percentage: {round(error_count * 2 / len(detected_ints), 2)} %")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # DEBUGGING!!!    
 
@@ -158,9 +207,9 @@ print(f"Transmission Symbol Errors: {error_count(b_k[len(header):-len(header)], 
 # plt.stem(np.imag(a_k_upsampled[len(header)*sample_rate:-len(header)*sample_rate]))
 # plt.title("Upsampled Signal")
 
-plt.figure()
-plt.stem(s_nT_imag)
-plt.title("Match Filtered Signal")
+# plt.figure()
+# plt.stem(s_nT_imag)
+# plt.title("Match Filtered Signal")
 
 # plt.figure()
 # plt.stem(symbol_block_imag)
@@ -171,7 +220,7 @@ plt.title("Match Filtered Signal")
 # plt.title("Symbol Block Upsampled")
 
 # plt.figure()
-# plt.stem(symbol_block_real_interpolated)
+# plt.stem(symbol_block_imag_interpolated)
 # plt.title("Symbol Block Interpolated")
 
 # plt.figure()
@@ -181,5 +230,9 @@ plt.title("Match Filtered Signal")
 # plt.figure()
 # plt.stem(s_nT_real_downsampled)
 # plt.title("TED Output Symbols")
+
+plt.figure()
+plt.stem(scs.get_timing_error())
+plt.title("Timing Error")
 
 plt.show()
