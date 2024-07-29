29b05de6327ddf9c3f61e352b404fd6b43b9f826
Powers <w656p201@home.ku.edu>
2024-07-29 16:55:11 -0500

    adding jupyter notebooks and stuff

---
+++ Jupyter/README.md
@@ -0,0 +1,6 @@
+
+
+
+## Quadrature Amplitude Modulation Review ##
+
+A fundamental understanding of QAM and other signal modulation standards is assumed and will be only briefly reviewed before model development is discussed. Quadrature Amplitude Modulation (QAM) is a method that combines amplitude modulation (AM) and phase modulation (PM) to encode data into a signal. It achieves this by varying the amplitude of two carrier waves, which are 90 degrees out of phase with each other. This allows QAM to represent multiple bits of information per symbol, increasing data transmission rates. Common variations of QAM, such as 16-QAM and 64-QAM, use different combinations of amplitude and phase shifts to transmit 4 and 6 bits per symbol, respectively. The robustness of QAM makes it widely used in modern communication systems, including digital television and broadband internet. However, its susceptibility to noise and interference requires careful implementation and signal processing techniques to maintain signal integrity and optimize performance.
--- QPSK/main.py
+++ QPSK/main.py
@@ -26,10 +26,10 @@ def error_count(x, y):
 sample_rate = 8
 carrier_frequency = 0.25*sample_rate
 symbol_clock_offset = 0
-qpsk_constellation = [[complex( np.sqrt(4.5)+ np.sqrt(4.5)*1j), 3], 
-                      [complex( np.sqrt(4.5)+-np.sqrt(4.5)*1j), 2], 
-                      [complex(-np.sqrt(4.5)+-np.sqrt(4.5)*1j), 0], 
-                      [complex(-np.sqrt(4.5)+ np.sqrt(4.5)*1j), 1]]
+qpsk_constellation = [[complex( np.sqrt(1) +  np.sqrt(1)*1j), 3], 
+                      [complex( np.sqrt(1) + -np.sqrt(1)*1j), 2], 
+                      [complex(-np.sqrt(1) + -np.sqrt(1)*1j), 0], 
+                      [complex(-np.sqrt(1) +  np.sqrt(1)*1j), 1]]
 
 bits = [i[1] for i in qpsk_constellation]
 bits_str = ['11', '10', '00', '01']
@@ -40,7 +40,7 @@ bits_to_bits_str = dict(zip(bits, bits_str))
 
 header = [1, 1, 1, 1]
 
-test_input_1 = [1, 0, 0, 1]
+test_input_1 = [1, 0, 1, 0]
 test_input_2 = [3, 2, 1, 0, 1, 2, 3]
 string_input = "will is cool, this is a test"
 string_input_bin = ''.join(string_to_ascii_binary(string_input))
@@ -49,11 +49,13 @@ test_input_3 = [int(bin2, 2) for bin2 in input_bin_blocks]
 
 
 # 1.1 UPSAMPLE THE BASEBAND DISCRETE SYMBOLS
-b_k = header + test_input_3
+b_k = header + test_input_1
 a_k = [bits_to_amplitude[bit] for bit in b_k]
 a_k_upsampled = DSP.upsample(a_k, sample_rate, interpolate_flag=False)
 a_k_upsampled_real = np.real(a_k_upsampled)
 a_k_upsampled_imag = np.imag(a_k_upsampled)
+
+# DSP.plot_complex_points(a_k_upsampled[len(header)*sample_rate:], referencePoints=amplitudes) # plotting received constellations
 
 # 1.2 PULSE SHAPE THE UPSAMPLED SIGNAL (SRRC)
 length = 64
@@ -81,7 +83,7 @@ x_kTs_real = np.array(DSP.downsample(x_nT_real, sample_rate))
 x_kTs_imag = np.array(DSP.downsample(x_nT_imag, sample_rate))
 x_kTs = x_kTs_real + 1j * x_kTs_imag
 
-DSP.plot_complex_points(x_kTs, referencePoints=amplitudes) # plotting received constellations
+# DSP.plot_complex_points(x_kTs[len(header)*sample_rate:], referencePoints=amplitudes) # plotting received constellations
 
 # 2.4 MAKE A DECISION FOR EACH PULSE
 detected_ints = communications.nearest_neighbor(x_kTs[len(header):], qpsk_constellation)
@@ -95,35 +97,55 @@ for symbol in detected_ints:
 message = communications.bin_to_char(detected_bits)
 print(message)
 
-# # Plot original symbols
-# plt.figure()
-# plt.stem(np.imag(a_k))
-# plt.title("Original Symbols")
+# Plot original symbols
+plt.figure()
+plt.stem(np.imag(a_k[len(header):]))
+plt.title("Original Symbols")
+plt.xlabel("Sample Time [n]")
+plt.ylabel("Amplutide [V]")
 
-# # Plot upsampled symbols
-# plt.figure()
-# plt.stem(np.real(a_k_upsampled))
-# plt.title("Upsampled Symbols")
+# Plot upsampled symbols
+plt.figure()
+plt.stem(np.imag(a_k_upsampled[len(header)*sample_rate:]))
+plt.title("Upsampled Symbols")
+plt.xlabel("Sample Time [n]")
+plt.ylabel("Amplutide [V]")
 
-# # Plot modulated signal
-# plt.figure()
-# plt.stem(s_nT_modulated)
-# plt.title("Modulated Signal")
+# Plot match filtered signal
+plt.figure()
+plt.stem(s_nT_imag[len(header)*sample_rate:])
+plt.title("Pulse Shaped Signal")
+plt.xlabel("Sample Time [n]")
+plt.ylabel("Amplutide [V]")
 
-# # Plot demodulated signal
-# plt.figure()
-# plt.stem(r_nT)
-# plt.title("Demodulated Signal")
+# Plot modulated signal
+plt.figure()
+plt.stem(s_nT_modulated[len(header)*sample_rate:])
+plt.title("Modulated Signal")
+plt.xlabel("Sample Time [n]")
+plt.ylabel("Amplutide [V]")
 
-# # Plot match filtered signal
-# plt.figure()
-# plt.stem(x_nT)
-# plt.title("Match Filtered Signal")
+# Plot demodulated signal
+plt.figure()
+plt.stem(r_nT_imag[len(header)*sample_rate:])
+plt.title("Demodulated Signal")
+plt.xlabel("Sample Time [n]")
+plt.ylabel("Amplutide [V]")
 
-# # Plot downsampled signal
-# plt.figure()
-# plt.stem(np.imag(x_kTs))
-# plt.title("Downsampled Signal")
-# plt.show()
+# Plot match filtered signal
+plt.figure()
+plt.stem(x_nT_imag[len(header)*sample_rate:])
+plt.title("Match Filtered Signal")
+plt.xlabel("Sample Time [n]")
+plt.ylabel("Amplutide [V]")
+
+# Plot downsampled signal
+plt.figure()
+plt.stem(np.imag(x_kTs[len(header):]))
+plt.title("Downsampled Signal")
+plt.xlabel("Sample Time [n]")
+plt.ylabel("Amplutide [V]")
+
+plt.show()
 
 
